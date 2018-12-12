5fc7f915c9f991fb30276a7cb108a804153fa6b1
Tarak Shah <tarak@hrdag.org>
2018-12-12 12:49:51 -0800

    methods for interacting with and printing results

--- R/codeurl.R
+++ R/codeurl.R
@@ -14,16 +14,41 @@ sanitize <- function(codes) {
     list(code = code_type, section = code_num)
 }
 
+dl_url <- function(url) {
+    structure(xml2::read_html(url), orig_input = )
+}
+
 #' Download legislative info related to legal codes
 #' 
 #' @param codes A vector of legal codes
 #' @importFrom dplyr %>%
 #' @export
 leg_info <- function(codes) {
-    codes %>%
-        sanitize %>%
-        build_url %>%
-        map(xml2::read_html) %>%
-        map(~rvest::html_nodes(., "h4") %>% rvest::html_text()) %>%
-        map_chr(paste, collapse = "\n")
+    clean_codes <- sanitize(codes)
+    urls <- build_url(clean_codes)
+    dl_url <- function(url, index) {
+        structure(xml2::read_html(url),
+                  orig_input = codes[index])
+    }
+
+    structure(purrr::imap(urls, dl_url),
+              class = "ca_leg_info")
+}
+
+#' @export
+as.character.ca_leg_info <- function(x, ..., detail = FALSE) {
+    nodes <- if (detail) "p, h4, h5, h6" else "h4, h5, h6"
+    purrr::map(x, ~rvest::html_nodes(., nodes) %>% rvest::html_text()) %>%
+        purrr::map_chr(paste, collapse = "\n")
+}
+
+#' @export
+print.ca_leg_info <- function(x, ..) {
+    to_print <- stringr::str_wrap(as.character(x, detail = TRUE),
+                                  width =  80, exdent = 4, indent = 4)
+    disp <- function(index)
+        cat("\n", attr(x[[index]], "orig_input"), "~~~~~~~~~~~~~\n",
+            stringr::str_sub(to_print[[index]], 1, 350), "...\n")
+    purrr::map(seq_along(x), disp)
+    invisible(x)
 }
